81d031d78db5f1b7bffbf27a42cf3da66eb6df91
tekarukite <dostedavid@gmail.com>
2016-11-05 21:45:36 +0100

    leg
    con recurrencia

--- numerico/aprox_pol/nou_pol_Legendre.m
+++ numerico/aprox_pol/nou_pol_Legendre.m
@@ -1,5 +1,8 @@
 function res = nou_pol_Legendre(x, coefs)
 
+%sabem que al ser un polinomi de grau n ortotonal a una dimensio menys
+%aleshores compleix: P_(i+1)(x) = (a_i(x) + b_i)*P_i(x) + c_i*P_(i-1)(x);
+
 n = length(coefs);
 L = zeros(n,length(x));
 for i=1:length(x)
